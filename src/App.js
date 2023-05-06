040adfbbf6ca800e972667445a181f2799d0e8af
Justin A Oeters <justin.oeters@gmail.com>
2023-05-05 21:16:23 -0400

    final markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+const [text, setText] = useState(`
+# Welcome to Markdown Previewer
+## Fun with React
+[title](https://www.example.com)
+\`code\`
+\`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+
+- first
+- second
+- third
+
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+
+
+![alt text](image.jpg)
+
+**bold text**
+`);
+
+marked.setOptions({
+  breaks: true
+ })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea 
+      id="editor" 
+      onChange = {(event) => {
+  setText(event.target.value);
+  }} 
+  value={text}
+  ></textarea>
+<div 
+id="preview" 
+dangerouslySetInnerHTML={{
+  __html: marked.parse(text),
+}}>
+  </div>
     </div>
   );
 }
